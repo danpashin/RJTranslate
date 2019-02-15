d21f8cefb66c20646bef3d288d28a016ca3a56c2
Daniil Pashin <daniilpashin@icloud.com>
2019-02-15 13:03:07 +0300

    [f] Partitial collection view update

---
+++ RJTranslate-App/Models/NotificationConveniences.swift
@@ -0,0 +1,15 @@
+//
+//  NotificationPropertyModel.swift
+//  RJTranslate-App
+//
+//  Created by Даниил on 11/02/2019.
+//  Copyright © 2019 Даниил. All rights reserved.
+//
+
+import Foundation
+
+struct NotificationPropertyModel {
+    var oldValue: Any?
+    
+    var newValue: Any?
+}
